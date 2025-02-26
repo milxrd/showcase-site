addad4711428afbbf4efc78d82b44a32adde5bab
miloszlesiak <Kopli099@gmail.com>
2025-02-26 02:08:16 +0100

    Blog changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import { withContentlayer } from 'next-contentlayer'
 
 const config = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+      resolveAlias: {
+        underscore: 'lodash',
+        mocha: { browser: 'mocha/browser-entry.js' },
+      },
+      resolveExtensions: [
+        '.mdx',
+        '.tsx',
+        '.ts',
+        '.jsx',
+        '.js',
+        '.mjs',
+        '.json',
+      ],
+    },
+  },
 }
 
 export default withContentlayer(config);
